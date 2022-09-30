2d75ecc33db6deb521ceaab46026a452fce1c429
Lisa Nguyen <lisanguyensc@gmail.com>
2022-09-30 20:50:42 +0000

    Completed shopping-list feature and added functionality to form and ul elements

--- js/data.js
+++ js/data.js
@@ -1,1 +1,14 @@
 /* exported data */
+/* exported shoppingList */
+
+var shoppingList = [];
+var previousShoppingListJSON = localStorage.getItem('javascript-local-storage');
+
+if (previousShoppingListJSON !== null) {
+  shoppingList = JSON.parse(previousShoppingListJSON);
+}
+
+window.addEventListener('beforeunload', function (event) {
+  var shopListJSON = JSON.stringify(shoppingList);
+  localStorage.setItem('javascript-local-storage', shopListJSON);
+});
